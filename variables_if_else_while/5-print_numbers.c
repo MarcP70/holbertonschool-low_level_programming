7a4de69fe768e5520be982ffdc397d2e032455a1
MarcP70 <marcpourias@yahoo.fr>
2023-02-20 06:08:37 -0800

    Add file 5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/* more headers goes there */
+# include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Program entry point
+ * This program prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
