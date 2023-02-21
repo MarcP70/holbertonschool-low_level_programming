7d87fc57e4bf13320f5f042a7072f7e411434f2e
MarcP70 <marcpourias@yahoo.fr>
2023-02-21 01:29:22 -0800

    Add file 2-print_alphabet_x10.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <unistd.h>
-/** Function prototype _putchar */
-/** Print on caractere */
+/* Function prototype _putchar */
 int _putchar(char c);
-/** Function prototype print_alphabet */
+/* Function prototype print_alphabet */
 void print_alphabet(void);
+/* Function prototype print_alphabet_x10 */
+void print_alphabet_x10(void);
