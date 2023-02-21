1bd3640303f76a8914c32aa33e31ebbd58403796
MarcP70 <marcpourias@yahoo.fr>
2023-02-21 03:42:53 -0800

    Add file 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+/**
+ * _islower - function that checks for lowercase character
+ * @c: The character to check
+ *
+ * Return: 1 if charater is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,3 +5,5 @@ int _putchar(char c);
 void print_alphabet(void);
 /* Function prototype print_alphabet_x10 */
 void print_alphabet_x10(void);
+/* Function prototype _islower */
+int _islower(int c);
