c39a62a9acd0bbd9c82fd02f2ff33911966a751a
MarcP70 <marcpourias@yahoo.fr>
2023-02-27 05:32:12 -0800

    Add file 3-puts.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,14 @@
 /* Functions prototypes */
 
+/* _putchar */
+int _putchar(char c);
 /* Task 0 */
 void reset_to_98(int *n);
 /* Task 1 */
 void swap_int(int *a, int *b);
 /* Task 2 */
 int _strlen(char *s);
+/* Task 3 */
+void _puts(char *str);
+/* Task 4 */
+void print_rev(char *s);
