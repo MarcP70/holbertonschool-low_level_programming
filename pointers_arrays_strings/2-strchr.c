4b502c9bfb989873b5a803b46a52f61c03aa7a91
MarcP70 <marcpourias@yahoo.fr>
2023-03-02 09:07:17 -0800

    Add file 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,7 @@
  *           The function fills the first n bytes of the memory area
  *           pointed to by s with the constant byte b
  *
- * @s: pointer to the strin
+ * @s: pointer to the string
  * @c: caracter to locate
   *
  * Return: a pointer to the first occurrence of the character c in the string s
@@ -15,5 +15,13 @@
 
 char *_strchr(char *s, char c)
 {
+	int index;
 
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
 }
