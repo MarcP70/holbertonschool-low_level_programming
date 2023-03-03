cbf8f5fd85c96fd2b3e5e8f7ed0716ecf78f8ad6
MarcP70 <marcpourias@yahoo.fr>
2023-03-02 23:53:46 -0800

    Add file 3-strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn -  This function gets the length of a prefix substring
+ *
+ * @s: pointer to the string sourc
+ * @accept: pointer to the string allowed caracters
+  *
+ * Return: the number of bytes in the initial segment of s which consist
+ *         only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+
+	char *p, *q;
+
+
+	for (p = s; *p; p++)
+	{
+		for (q = accept; *q; q++)
+		{
+			if (*p == *q)
+			{
+				count++;
+				break;
+			}
+		}
+
+		if (*q == '\0')
+			return (count);
+	}
+
+	return (count);
+}
