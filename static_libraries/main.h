f5224c3f781eaa674cb319830922b2d455a7fcde
MarcP70 <marcpourias@yahoo.fr>
2023-03-07 01:15:38 -0800

    Prevent Multiple and Circular Inclusion of Header Files

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 /* Functions prototypes */
 
 /* _putchar */
@@ -23,3 +25,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
