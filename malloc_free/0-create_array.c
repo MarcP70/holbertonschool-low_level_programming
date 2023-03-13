da2dbbe073d93cbc75db3df0ce831243b9ba9118
MarcP70 <marcpourias@yahoo.fr>
2023-03-13 03:45:20 -0700

    Control right allocation

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,8 +14,15 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	ar = malloc(size * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		ar[i] = c;
+
 	return (ar);
 }
