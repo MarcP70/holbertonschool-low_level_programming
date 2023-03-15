322b71674e16a287e31ad71a7fcc9267e43ad26f
MarcP70 <marcpourias@yahoo.fr>
2023-03-15 03:27:07 -0700

    Modify the size of malloc

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = malloc((max + 1) * sizeof(int *));
+	ar = malloc((max - min + 1) * sizeof(int));
 	if (ar == NULL)
 		return (NULL);
 
