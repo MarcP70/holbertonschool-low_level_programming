fc497b6719f6346d14630a41c2f6955fdaa2c835
MarcP70 <marcpourias@yahoo.fr>
2023-03-20 02:57:30 -0700

    Create README and add file dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,2 @@
+C - Structures, typedef
+20/03/2023
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog description
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ *
+ * Description: Differents elements to categorize a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
