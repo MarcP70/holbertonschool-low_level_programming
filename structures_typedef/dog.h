992828e28dce3226b12c33e9f7f098259d616344
MarcP70 <marcpourias@yahoo.fr>
2023-03-20 06:22:16 -0700

    Add a new alias of struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,12 +10,12 @@
  *
  * Description: Differents elements to categorize a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 /* Functions prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
