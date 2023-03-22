d3faa85d654706cc70d4f56b1b85f86b5ff5bd98
MarcP70 <marcpourias@yahoo.fr>
2023-03-22 06:32:24 -0700

    Correction betty style

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,21 +5,25 @@
  * get_op_func - This function selects the correct function
  *               to perform the operation asked by the user
  * @s: the operator passed as argument to the program
- * @a: the fist argument
- * @b: the second argument
  * Return: the pointer to the function that corresponds to the
  *         operator given as a parameter
  */
-int (*get_op_func(char *s))(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	{
+		i++;
+	}
+
+	return (ops[i].f);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -20,7 +20,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-        return (a - b);
+	return (a - b);
 }
 
 /**
@@ -42,7 +42,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-        return (a / b);
+	return (a / b);
 }
 
 /**
@@ -51,7 +51,7 @@ int op_div(int a, int b)
  * @b: the second integer
  * Return: the remainder of the division
  */
-int op_sub(int a, int b)
+int op_mod(int a, int b)
 {
-        return (a % b);
+	return (a % b);
 }
