a74c1230f2b38db4461d47deb4cdb22682ee41cd
MarcP70 <marcpourias@yahoo.fr>
2023-03-23 08:53:04 -0700

    Added condition if NULL

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -8,12 +8,15 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d->owner != NULL)
-		free(d->owner);
-
-	if (d->name != NULL)
-		free(d->name);
-
 	if (d != NULL)
-		free(d);
+	{
+		if (d->owner != NULL)
+			free(d->owner);
+
+		if (d->name != NULL)
+			free(d->name);
+
+		if (d != NULL)
+			free(d);
+	}
 }
