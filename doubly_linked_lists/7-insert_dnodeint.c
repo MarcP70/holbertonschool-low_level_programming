71b1e569dabe17424e31e19cbb84136dc9a6c05e
MarcP70 <marcpourias@yahoo.fr>
2023-04-05 05:42:44 -0700

    Add description for parameter 'n' and correcting open brace

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,6 +7,7 @@
  *
  * @h: A pointer to pointer of the first element of the list.
  * @idx: The index of the list where the new node should be added.
+ * @n: The data of the new node.
  *
  * Return: The address of the new node, or NULL if it failed.
  */
@@ -19,30 +20,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
 	/* Insert the node at the beginning of the list */
-	if (idx == 0) {
+	if (idx == 0)
+	{
 		if (*h != NULL)
 			(*h)->prev = new_node;
 		new_node->next = *h;
 		*h = new_node;
 		return (new_node);
 	}
-
 	/* Traverse the list until the desired index is reached */
 	for (i = 0; i < idx - 1 && temp_node != NULL; i++)
 		temp_node = temp_node->next;
-
 	/* If the index is out of bounds, free the new node and return NULL */
-	if (temp_node == NULL) {
+	if (temp_node == NULL)
+	{
 		free(new_node);
 		return (NULL);
 	}
-
 	/* Insert the node between two existing nodes */
 	new_node->next = temp_node->next;
 	if (temp_node->next != NULL)
