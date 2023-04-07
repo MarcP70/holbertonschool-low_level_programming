09ecf87b2dc1216fbf3f40550310b1823884044e
MarcP70 <marcpourias@yahoo.fr>
2023-04-07 07:10:18 -0700

    Add a cast UL for long unsigned int

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -5,7 +5,7 @@
  * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: the decimal to be subtracted.
  * @index: the index of the bit to change.
- *
+ * 1UL: cast the expression "1" to a long unsigned int
  * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
@@ -14,12 +14,12 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index < 32)
 	{
 		/* check power < *n */
-		if ((1 << index) <= *n)
+		if ((1UL << index) <= *n)
 		{
 			if (*n > 0)
 			{
 				/* 1 << index = 1 x 2^index */
-				*n = *n - (1 << index);
+				*n = *n - (1UL << index);
 			}
 			else
 				return (0);
