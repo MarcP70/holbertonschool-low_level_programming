793ae9b53e879bacc5e52dac2d98568c59abea03
MarcP70 <marcpourias@yahoo.fr>
2023-04-14 05:24:07 -0700

    Add loop for initialization the arrays at NULL

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,6 +11,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
@@ -26,6 +27,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash_table);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 
 	return (hash_table);
 }
