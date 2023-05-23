8f1e8eef426c6927e84f4f2da5b573048d95513c
MarcP70 <marcpourias@yahoo.fr>
2023-05-23 08:43:21 -0700

    Delete: a printf for debug

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
-		printf("mid:%ld\n", mid);
+
 		printf("Searching in array: ");
 
 		for (i = left; i <= right; i++)
